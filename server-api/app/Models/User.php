0502ef0480f369fe0c2d71382703791c19706f97
Kenwrick Espinosa <kenwrickespinosa@gmail.com>
2026-01-01 15:48:30 +0800

    feat(models): add Story model for stories table
    - Implemented relationship between the Story and User model
    
    - Stories belongs to user
    
    - User has many story

--- server-api/app/Models/User.php
+++ server-api/app/Models/User.php
@@ -4,13 +4,15 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -18,10 +20,18 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'firstname',
+        'lastname',
+        'gender',
+        'birthdate',
         'email',
         'password',
     ];
+
+    public function stories(): HasMany
+    {
+        return $this->hasMany(Story::class);
+    }
 
     /**
      * The attributes that should be hidden for serialization.
