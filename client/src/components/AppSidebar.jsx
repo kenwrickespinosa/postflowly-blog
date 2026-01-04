01c0b07443b7ebe973af6f9ec046b7f221e2b0f0
Kenwrick Espinosa <kenwrickespinosa@gmail.com>
2026-01-05 00:48:29 +0800

    feat(logout): implement logout

--- client/src/components/AppSidebar.jsx
+++ client/src/components/AppSidebar.jsx
@@ -8,7 +8,16 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "./ui/sidebar";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 
 const items = [
   {
@@ -22,11 +31,34 @@ const items = [
 ];
 
 function AppSidebar() {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const res = await fetch("http://127.0.0.1:8000/api/auth/logout", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        }
+      })
+
+      const data = await res.json();
+
+      if (!res.ok) throw new Error(data.message || "Failed to logout");
+
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <Sidebar>
       <SidebarGroup>
         <SidebarGroupLabel>Postflowly</SidebarGroupLabel>
-        <SidebarGroupContent>
+        <SidebarGroupContent className="flex flex-col gap-8">
           <SidebarMenu>
             {items.map((item) => (
               <SidebarMenuItem key={item.title}>
@@ -34,13 +66,40 @@ function AppSidebar() {
                   <NavLink
                     to={item.url}
                     end={false}
-                    className={({isActive}) => `w-full ${isActive ? "text-blue-400 w-full font-bold" : "text-inherit w-full"}`}
+                    className={({ isActive }) =>
+                      `w-full font-inter ${
+                        isActive
+                          ? "text-green-600 w-full font-bold"
+                          : "text-inherit w-full"
+                      }`
+                    }
                   >
                     {item.title}
                   </NavLink>
                 </SidebarMenuButton>
               </SidebarMenuItem>
             ))}
+          </SidebarMenu>
+          <SidebarMenu>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button
+                  className="flex justify-start pl-2 bg-inherit text-black font-normal
+                    cursor-pointer font-inter hover:bg-neutral-100"
+                >
+                  Logout
+                </Button>
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle className="font-inter mt-6">Are you sure you want to logout?</DialogTitle>
+                  <DialogDescription className="sr-only" />
+                </DialogHeader>
+                <div className="flex justify-end">
+                  <Button onClick={handleLogout} className="font-inter w-25 cursor-pointer">Yes</Button>
+                </div>
+              </DialogContent>
+            </Dialog>
           </SidebarMenu>
         </SidebarGroupContent>
       </SidebarGroup>
--- client/src/components/StoryCard.jsx
+++ client/src/components/StoryCard.jsx
@@ -1,24 +1,57 @@
-import React from 'react'
-import StoryContent from './editor/StoryContent';
+import React from "react";
+import { FaCalendar } from "react-icons/fa";
 
 function StoryCard({ story }) {
-  const getDate = () => {
-    return story.created_at.split("T")[0];
-  }
+  // Format date to string
+  const formatDate = () => {
+    if (!story.created_at) return "";
+
+    const date = new Date(story.created_at); // keep full timestamp
+
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+
+    return date.toLocaleDateString("en-PH", options);
+  };
 
   return (
-    <div>
-      <div>{story.firstname} {story.lastname}</div>
-      <div>
-        <div>
-          <span>{story.title}</span>
-          <div><StoryContent content={story.content} /></div>
+    <div className="p-4 md:px-8">
+      <div className="grid grid-cols-1 gap-2 md:grid-cols-2 md:gap-12">
+        {/* Image */}
+        <div className="order-1 md:order-2 flex justify-center md:justify-end">
+          <div className="w-full h-40 md:h-48 bg-neutral-200 rounded">
+            img here
+          </div>
         </div>
-        <div>img here</div>
+
+        {/* Content */}
+        <div className="order-2 md:order-1 flex flex-col gap-2">
+          <p>
+            {story.firstname} {story.lastname}
+          </p>
+
+          <p className="font-inter text-lg md:text-2xl">{story.title}</p>
+
+          <p
+            className="font-inter text-sm text-neutral-600 
+                      line-clamp-3 md:line-clamp-4 md:text-base"
+          >
+            {story.excerpt}
+          </p>
+
+          <div className="flex gap-1 mt-4">
+            <FaCalendar className="text-xs text-neutral-600" />
+            <p className="font-inter text-xs text-neutral-600">
+              {formatDate()}
+            </p>
+          </div>
+        </div>
       </div>
-      <div>{getDate()}</div>
     </div>
-  )
+  );
 }
 
-export default StoryCard
+export default StoryCard;
